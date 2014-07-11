08a7223e51d228b92455526dd05f410a96884f94
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-07-11 15:01:06 -0700

    final-ish

--- app/index.js
+++ app/index.js
@@ -2,22 +2,17 @@ var express = require('express');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-//var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
-var flash = require('connect-flash');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes');
 
-require('./auth.js')(passport);
 var app = express();
 
 var connection = mongoose.connect('mongodb://localhost/api');
 
 
 app.use(logger('dev'));
-//app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -29,11 +24,13 @@ app.use(passport.session());
 
 
 
+/*
 app.use(function(req, res, next){
     console.log(req);
     return next();
 });
 
+*/
 app.use('/', routes);
 //app.use(express.static(__dirname + '/../dist'));
 app.listen(8000);
